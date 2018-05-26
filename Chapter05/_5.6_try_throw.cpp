0a6c8795a8fcc7b33db454ab40718a212f180195
sixologyyy <sixologyyy@icloud.com>
2018-05-26 17:52:22 +0800

    add _5.6_try_throw

---
+++ Chapter05/_5.6_try_throw.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stdexcept>
+using namespace std;
+int main()
+{
+	
+	int a, b;
+
+	while (1)
+	{
+		try
+		{
+			cout << "输入被除数和除数" << endl;
+			cin >> a >> b;
+
+			if (b!=0)
+			{
+				cout << a << "/" << b << "=" << a / b << endl;
+				break;
+			}
+			else
+			{
+				//cerr << "两个数不相等" << endl;
+				throw runtime_error("除数不能为0");
+			}
+
+		}
+		catch (runtime_error err)
+		{
+			cout << err.what() << endl
+				<< "try again y/n?";
+			char c;
+			cin >> c;
+			if (!cin || c == 'n')
+				break;
+		}
+	}
+
+
+
+}
