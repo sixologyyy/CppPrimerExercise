9f2f2edb5e8523e12beab504d18712d9bbc6969c
sixologyyy <sixologyyy@icloud.com>
2018-05-30 16:20:59 +0800

    Update _7.3_class_member.cpp

--- Chapter07/_7.3_class_member.cpp
+++ Chapter07/_7.3_class_member.cpp
@@ -6,6 +6,12 @@ class Screen
 public:
 	typedef std::string::size_type pos;    //类型成员需要在使用前定义，普通成员则不必
 
+        //友元类的成员可以访问此类的所有成员
+	friend class WindowMgr;
+	//或者仅声明其他类中的友元函数，但是此前必须有WindowMgr类的定义，其中clear()又用到了Screen类
+	//所以要，先定义WindowMgr类，在里面声明Screen类，随后声明clear()函数，再定义Screen类，在里面声明WMgr::clear()友元，最后定义clear()，此时才可以使用Screen的成员
+	//friend void WindowMgr::clear(screenIndex);
+
 	Screen() = default;
 	Screen(pos ht, pos wd) :
 		height(ht), width(wd), contents(ht*wd, ' ') {}
@@ -13,7 +19,7 @@ public:
 		height(ht),width(wd),contents(ht*wd,c) {}
 
 	//读取光标处的字符，类内部定义的函数是隐式inline
-	char get() const              
+	char get() const
 		{ return contents[cursor]; }
 	//显式声明inline，get的重载版本
 	inline char get(pos ht, pos wd) const;
@@ -21,7 +27,7 @@ public:
 	Screen& set(char);
 	Screen& set(pos, pos, char);
 	//移动光标，可以在之后定义的时候设为内联
-	Screen& move(pos r, pos c); 
+	Screen& move(pos r, pos c);
 	//调用显示函数，有常量和非常量版本，非常量版本是为了链式操作
 	//调用非常量版本时，display隐式地传递this指针给doDisplay，后者隐式地把指向非常量的指针转换为指向常量的指针
 	Screen& display(std::ostream& os)
@@ -29,7 +35,7 @@ public:
 	const Screen& display(std::ostream& os) const        //注意，第一个const并不能区分重载，因为仅有返回值不同
 		{ doDisplay(os); return *this; }
 	//记录成员函数被调用的次数
-	void someMember() const;      
+	void someMember() const;
 
 private:
 	pos cursor = 0;
@@ -39,15 +45,23 @@ private:
 	mutable size_t accessCtr;      //mutable即使在const对象、方法中中也是可变的
 
 	//负责显示
-	void doDisplay(std::ostream& os) const { os << contents; }
+	void doDisplay(std::ostream& os) const { os << contents; }     //要声明为const成员，否则常量版本不能调用
 };
 
-//用于追踪Screen
+//窗口类，用于追踪Screen
 class WindowMgr
 {
-//默认情况下包含一个标准尺寸的空白Screen
 private:
+    //默认情况下包含一个标准尺寸的空白Screen
 	std::vector<Screen> screens{ Screen(24,80,' ') };
+
+public:
+    //窗口中每个屏幕的编号
+    using screenIndex=std::vector<Screen>::size_type;
+    //按照编号将指定creen置为空白
+    void clear(screenIndex);
+    //向窗口添加屏幕，返回屏幕编号
+    screenIndex addScreen(const Screen&);
 };
 
 inline Screen& Screen::move(pos r, pos c)
@@ -80,6 +94,18 @@ void Screen::someMember() const
 	++accessCtr;
 }
 
+void WindowMgr::clear(screenIndex i)
+{
+    Screen& s=screens[i];
+    s.contents=std::string(s.height*s.width,' ');
+}
+
+WindowMgr::screenIndex WindowMgr::addScreen(const Screen& s)     //返回类型如果是某个类的成员，需要指明，因为它在函数名之前，此时并没有进入类的作用域
+{
+    screens.push_back(s);
+    return screens.size()-1;
+}
+
 int main()
 {
 	Screen myScreen(5,10,'_');
@@ -89,10 +115,12 @@ int main()
 	ch = myScreen.get(0, 0);
 
 	myScreen.move(4, 0).set('#');
-	myScreen.display(std::cout);
+	myScreen.display(std::cout).move(4, 1).set('*');
 	std::cout << std::endl;
+	myScreen.display(std::cout);
+        std::cout << std::endl;
 
 	cScreen.display(std::cout);
-	std::cout << std::endl;;
+	std::cout << std::endl;
 
 }
