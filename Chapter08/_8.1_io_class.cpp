8673468f2197defd7617abe4d11cf616fe7d6e15
sixologyyy <sixologyyy@icloud.com>
2018-05-31 21:36:23 +0800

    add _8.1_io_class.cpp

---
+++ Chapter08/_8.1_io_class.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+istream& func(istream &is)
+{
+	int buf;
+	while (is >> buf)
+		cout << buf << ends;     //ends插入一个空字符并刷新缓冲区
+	cout << "iostate: " << is.rdstate() << endl;
+	cout << flush;                  //flush刷新缓冲区，不附加内容
+	is.clear();
+	return is;
+}
+
+int main()
+{
+	istream& is = func(std::cin);
+	cout << "iostate: " << is.rdstate() << endl;
+	return 0;
+
+}
