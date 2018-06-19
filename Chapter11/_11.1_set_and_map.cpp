e73f8c6579461f3b4e64a23254b401abbcd18b94
sixologyyy <sixologyyy@icloud.com>
2018-06-19 22:01:40 +0800

    add _11.1_set_and_map.cpp

---
+++ Chapter11/_11.1_set_and_map.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+using namespace std;
+
+int main()
+{
+	map<string, int> wordCount;
+	set<string> exclude{"The","But","And","Or","An","A",
+									"the","but","and","or","an","a"};
+
+	string word;
+	while (cin >> word)
+	{
+		if(exclude.find(word)==exclude.end())
+			++wordCount[word];
+	}
+	for (auto i : wordCount)
+		cout << "word " << '\"' << i.first << '\"' << " occurs " << i.second << " times" << endl;
+}
